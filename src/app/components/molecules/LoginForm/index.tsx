505acce289fddd25c56c48ad633549b9da722ad6
Rafael de Oliveira <rafael.oliveira@infocaltechnologies.com>
2024-11-20 08:06:07 -0300

    fix: normalize inputs

--- src/app/components/molecules/LoginForm/index.tsx
+++ src/app/components/molecules/LoginForm/index.tsx
@@ -1,14 +1,12 @@
-// LoginForm.tsx
-'use client';
+"use client";
 
-import InputField from '../InputField';
-import Button from '../../atoms/Button';
-import React from 'react';
-import Form from '../../atoms/Form';
-import { Box } from '@chakra-ui/react';
-import AuthLink from '../AuthLink';
-import { LoginFormProps } from '@/shared/interfaces/login-form-props';
-import Spinner from '../Spinner';
+import InputField from "../InputField";
+import Button from "../../atoms/Button";
+import React from "react";
+import Form from "../../atoms/Form";
+import { Box } from "@chakra-ui/react";
+import { LoginFormProps } from "@/shared/interfaces/login-form-props";
+import Spinner from "../Spinner";
 
 const LoginForm: React.FC<LoginFormProps> = ({
   username,
@@ -16,7 +14,7 @@ const LoginForm: React.FC<LoginFormProps> = ({
   onUsernameChange,
   onPasswordChange,
   onSubmit,
-  isLoading = false
+  isLoading = false,
 }): React.JSX.Element => {
   const isFormValid = username.trim().length > 0 && password.trim().length >= 0;
 
@@ -25,40 +23,41 @@ const LoginForm: React.FC<LoginFormProps> = ({
       <Form onSubmit={onSubmit}>
         <Box>
           <InputField
-            id='username'
+            id="username"
             value={username}
-            onChange={onUsernameChange}
-            iconType='email'
-            placeholder='Digite seu email'
+            onChange={(value) =>
+              onUsernameChange({
+                target: { value },
+              } as React.ChangeEvent<HTMLInputElement>)
+            }
+            iconType="email"
+            placeholder="Digite seu email"
           />
           <InputField
-            id='password'
+            id="password"
             value={password}
-            onChange={onPasswordChange}
-            iconType='password'
-            placeholder='Digite sua senha'
+            onChange={(value) =>
+              onPasswordChange({
+                target: { value },
+              } as React.ChangeEvent<HTMLInputElement>)
+            }
+            iconType="password"
+            placeholder="Digite sua senha"
           />
           <Button
-            type='submit'
-            className='w-full mt-4'
-            buttonType='primary'
+            type="submit"
+            className="w-full mt-4"
+            buttonType="primary"
             isLoading={isLoading}
             isDisabled={!isFormValid}
           >
             Login
           </Button>
-
-          <AuthLink
-            preText='Não tem uma conta?'
-            text='Cadastre-se'
-            href='/register'
-          />
         </Box>
       </Form>
       {isLoading && (
-        <Box className='absolute flex justify-center items-center bg-[#00000030] w-full h-full top-0 bottom-0 right-0 left-0 z-40'>
-          {/* <Spinner className='absolute border border-red-800 top-1/2 bottom-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 z-50' /> */}
-          <Spinner className='relative z-50' width='50px' height='50px' />
+        <Box className="absolute flex justify-center items-center bg-[#00000030] w-full h-full top-0 bottom-0 right-0 left-0 z-40">
+          <Spinner className="relative z-50" width="50px" height="50px" />
         </Box>
       )}
     </>
