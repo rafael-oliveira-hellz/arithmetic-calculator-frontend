f0e854bda94b4ce60627d90728a070e5dccc413d
Rafael de Oliveira <rafael.oliveira@infocaltechnologies.com>
2024-11-21 22:30:08 -0300

    fix(pagination): adjusts current page

--- src/app/components/molecules/PaginationControls/index.tsx
+++ src/app/components/molecules/PaginationControls/index.tsx
@@ -31,7 +31,7 @@ const PaginationControls: React.FC<PaginationControlsProps> = ({
         Previous Page
       </Button>
       <Text>
-        Page {currentPage + 1} of {totalPages}
+        Page {currentPage} of {totalPages}
       </Text>
       <Button
         onClick={onNext}
