9497e9cd9acd520ac87f7268859268e787c88dff
Rafael de Oliveira <rafael.oliveira@infocaltechnologies.com>
2024-11-23 19:29:45 -0300

    refactor(table): centralize text alignment in <th> elements using CenteredRow component
    - Refactored the <CenteredRow> component to apply `textAlign: center` and `verticalAlign: middle` styles automatically to all <th> elements within a <Tr>.
    - Removed repetitive text alignment styles from individual <th> elements in the table header.
    - Improved reusability and maintainability of table styling logic.

--- src/app/components/atoms/CenteredRow/index.tsx
+++ src/app/components/atoms/CenteredRow/index.tsx
@@ -7,6 +7,12 @@ interface CenteredRowProps {
 const CenteredRow = ({ children }: CenteredRowProps): React.JSX.Element => (
   <Tr
     sx={{
+      "& > th": {
+        textAlign: "center",
+        verticalAlign: "middle",
+        padding: "8px",
+        borderBottom: "2px solid #FFF",
+      },
       "& > td": {
         textAlign: "center",
         verticalAlign: "middle",
