ed2e2f7f3838e230151a393957975eee88ccfe1b
Rafael de Oliveira <rafael.oliveira@infocaltechnologies.com>
2024-11-25 22:20:50 -0300

    fix(ui): adjust sidebar position

--- src/app/(pages)/records/page.tsx
+++ src/app/(pages)/records/page.tsx
@@ -2,24 +2,24 @@
 
 import React from "react";
 import { Box } from "@chakra-ui/react";
-// import AuthGuard from "@/app/components/guards/auth-guard";
+import AuthGuard from "@/app/components/guards/auth-guard";
 import RecordsTable from "@/app/components/organisms/RecordsTable";
 
 const RecordsPage: React.FC = () => {
   return (
-    // <AuthGuard>
-    <Box
-      bg="gray.800"
-      borderRadius="md"
-      boxShadow="md"
-      marginInline="6"
-      mt="6"
-      maxWidth="100%"
-      minHeight={"calc(100vh - 180px)"}
-    >
-      <RecordsTable />
-    </Box>
-    // </AuthGuard>
+    <AuthGuard>
+      <Box
+        bg="gray.800"
+        borderRadius="md"
+        boxShadow="md"
+        marginInline="6"
+        mt="6"
+        maxWidth="100%"
+        minHeight={"calc(100vh - 180px)"}
+      >
+        <RecordsTable />
+      </Box>
+    </AuthGuard>
   );
 };
 
